e42bed8c6e5375d7b26316027da4d46e7212f6c1
rajenshrestha@hotmail.com <rajen.shrestha@icloud.com>
2020-05-20 13:38:32 +1000

    Updated last message

--- components/logn-screen.tsx
+++ components/logn-screen.tsx
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import { ScrollView, StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function LoginScreen(props: object){
-    const[fname, setFName] = useState('');
-    const[lname, setLName] = useState('');
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
 
-    const {footer, main} = props.route.params;
+    const {footer, main, setLogin} = props.route.params;
 
     function goHome(){
       props.navigation.navigate('Home');
     }
 
-    function onClick(){
-        alert (`Hi Mate: ${lname}, ${fname}`);
+    function onLogin(){
+        if(username === 'rajen' && password == 'hello1'){
+          setLogin(true);
+          goHome();
+        }
+        else{
+          setLogin(false);
+        }
     }
+
     return (
       <View style={styles.container}>
         <View style={main}>
@@ -21,17 +28,17 @@ export default function LoginScreen(props: object){
           <Text style={styles.label}>Username:</Text>
                
           <TextInput style={styles.textBox} 
-            defaultValue={fname} 
-            onChangeText={t => setFName(t)}></TextInput>
+            defaultValue={username} 
+            onChangeText={t => setUsername(t)}></TextInput>
                 
           <Text style={styles.label}>Password:</Text>
                
           <TextInput style={styles.textBox} 
-            defaultValue={lname}
-            onChangeText={t => setLName(t)}></TextInput>
+            defaultValue={password}
+            onChangeText={t => setPassword(t)}></TextInput>
                 
           <View style={styles.buttonsContainer}>
-            <Button onPress={()=>{onClick()}} title='Login'></Button>
+            <Button onPress={()=>{onLogin()}} title='Login'></Button>
             <Button onPress={()=>{goHome()}} title='Cancel'></Button>
           </View>
         </View>
